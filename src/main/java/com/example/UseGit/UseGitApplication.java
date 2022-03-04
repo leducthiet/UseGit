ea48d2490cac84a7cab10d9ca85c31a57f57a0d0
DUCTHIET <leducthietchinh@gmail.com>
2022-03-04 19:47:26 +0700

    Initial commit

---
+++ src/main/java/com/example/UseGit/UseGitApplication.java
@@ -0,0 +1,16 @@
+package com.example.UseGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseGitApplication.class, args);
+		/**
+		 * Sau đây là một vài câu lệnh java simple.
+		 */
+	}
+
+}
---
+++ src/test/java/com/example/UseGit/UseGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UseGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
