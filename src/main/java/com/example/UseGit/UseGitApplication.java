50fabd4d3a6a0deb4c67eeb55561b6c6c55ed7e4
taokhongphailaTHIET <leducthietchinh@gmail.com>
2022-03-05 06:49:01 +0700

    CIA

--- src/main/java/com/example/UseGit/UseGitApplication.java
+++ src/main/java/com/example/UseGit/UseGitApplication.java
@@ -8,6 +8,6 @@ public class UseGitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UseGitApplication.class, args);
-		System.out.println("FBI FBIFBIFBIFBIFBIFBIFBIFBIFBIFBIFBIFBIFBI");
+		System.out.println("CIACIACIACIACIACIACIACIACIA");
 	}
 }
