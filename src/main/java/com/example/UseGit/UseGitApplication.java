3d8a02eaeac71fe7cf10df22b7434c40d79ebf72
RK_ThietLD <thietld_tts@rikkeisoft.com>
2022-03-07 21:33:14 +0700

    Lay ra Ham(Method)

--- src/main/java/com/example/UseGit/UseGitApplication.java
+++ src/main/java/com/example/UseGit/UseGitApplication.java
@@ -4,48 +4,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class UseGitApplication {
 
-	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException,
+			NoSuchMethodException, InvocationTargetException {
 		SpringApplication.run(UseGitApplication.class, args);
 
-		/**
-		 * Khoi tao doi tuong Girl
-		 */
-		Girl girl = new Girl();
-		girl.setName("Ngoc Trinh");
+		Class<Girl> girlClass = Girl.class;
 
 		/**
-		 * Lay ra tat ca field cua object
+		 * Su dung getDeclaredMethods de lay ra nhung method cua class va cha no.
 		 */
-		for (Field field:
-			 girl.getClass().getDeclaredFields()) {
+		Method[] methods = girlClass.getDeclaredMethods();
+		for (Method method:
+			 methods) {
 			System.out.println();
-			System.out.println("Field:" + field.getName());
-			System.out.println("Type" + field.getType());
+			System.out.println("Method: " + method.getName());
+			System.out.println("Parameters: " + Arrays.toString(method.getParameters()));
 		}
 
 		/**
-		 * Phan chinh
+		 * Lay ra method ten la setName va co 1 tham so truyen vao
+		 * => chinh la: setName(String name)
 		 */
+		Method methodSetName = girlClass.getMethod("setName", String.class);
 		/**
-		 * Lay ra field co ten 'name' (neu khong tim thay, no se ban NoSuchFieldException)
+		 * Bay gio methodSetName se dai dien cho method setName(String name) cua moi object co class la Girl
 		 */
-		Field nameField = girl.getClass().getDeclaredField("name");
-		/**
-		 * Cho phep truy cap tam thoi.(Vi no dang la private ma)
-		 */
-		nameField.setAccessible(true);
 
+		Girl girl = new Girl();
 		/**
-		 * Bay gio cai "nameField" dai dien cho thuoc tinh "name" cua moi object co class Girl.
-		 * Thay gia tri moi cua girl bang nameField
+		 * Thuc hien ham setName() tren doi tuong girl, gia tri truyen vao la "Ngoc Trinh"
 		 */
-		nameField.set(girl, "Sexy Pig");
-
+		methodSetName.invoke(girl,"Ngoc Trinh");
 		System.out.println(girl);
-
 	}
 }
